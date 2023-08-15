814334f89c2e7e753b517990e45b897c57f51406
Holberton1990 <ersida9019@gmail.com>
2023-08-15 01:23:35 -0700

    Simple shell

---
+++ simple_shell.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+void execute_command(char *command);
+#endif
