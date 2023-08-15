9d5c3071592780b80cd1d2a5648ed449a74272ee
Arlind3553 <georgetesco@yahoo.com>
2023-08-15 04:39:06 -0700

    task 1

---
+++ functions.c
@@ -0,0 +1,8 @@
+#include "main.h"
+char **identify_string(char *parameter)
+{
+	char **buf = maloc(sizeof(char *) * 1024);
+	char *split;
+	int i = 0;
+	char *delmitator = " \n"
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd>
+
+extern char **environ;
+
+#endif
