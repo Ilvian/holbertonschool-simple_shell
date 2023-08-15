40d2b16c5ff4288dbd08a0e93645fb046e4fb22b
Holberton1990 <ersida9019@gmail.com>
2023-08-15 02:49:50 -0700

    simple_shell

--- simple_shell.h
+++ simple_shell.h
@@ -1,10 +1,43 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-void execute_command(char *command);
-#endif
+int main(void)
+{
+    char command[MAX_COMMAND_LENGTH];
+    shell_session_t session = {0};
+    while (1)
+    {
+        printf("#cisfun$ ");
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        {
+            printf("\n");
+            exit(0);
+        }
+        command[strcspn(command, "\n")] = '\0';
+        if (strlen(command) == 0)
+            continue;
+        execute_command(command, &session);
+    }
+    return 0;
+}
+void execute_command(char *command, shell_session_t *session)
+{
+    pid_t pid;
+    int status;
+    if ((pid = fork()) == -1)
+    {
+        perror("Error forking");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        char *argv[] = {command, NULL};
+        if (execve(command, argv, environ) == -1)
+        {
+            perror("./shell");
+            exit(1);
+        }
+    }
+    else
+    {
+        wait(&status);
+        session->command_count++;
+    }
+}
