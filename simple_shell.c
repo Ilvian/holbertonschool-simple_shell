a98720c8653478ad939ddc674559e85a5dad92a6
Holberton1990 <ersida9019@gmail.com>
2023-08-15 03:02:34 -0700

    simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,15 +1,44 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#define MAX_COMMAND_LENGTH 256
-extern char **environ;
-typedef struct shell_session {
-    int command_count;
-} shell_session_t;
-void execute_command(char *command, shell_session_t *session);
-#endif
+#include "simple_shell.h"
+int main(void)
+{
+    char command[MAX_COMMAND_LENGTH];
+    shell_session_t session = {0};
+    while (1)
+    {
+        printf("#cisfun$ ");
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        {
+            printf("\n");
+            exit(0);
+        }
+        command[strcspn(command, "\n")] = '\0';
+        if (strlen(command) == 0)
+            continue;
+        execute_command(command, &session);
+    }
+    return 0;
+}
+void execute_command(char *command, shell_session_t *session)
+{
+    pid_t pid;
+    int status;
+    if ((pid = fork()) == -1)
+    {
+        perror("Error forking");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        char *argv[] = {command, NULL};
+        if (execve(command, argv, environ) == -1)
+        {
+            perror("./shell");
+            exit(1);
+        }
+    }
+    else
+    {
+        wait(&status);
+        session->command_count++;
+    }
+}
