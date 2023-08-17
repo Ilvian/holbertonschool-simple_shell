1e55742d7c45097f41a97b4f999607d135778ef7
Arlind3553 <georgetesco@yahoo.com>
2023-08-17 02:17:13 -0700

    task 1

--- myshell.c
+++ myshell.c
@@ -43,7 +43,7 @@ int main() {
         } else if (pid == 0) {
             execvp(args[0], args);
             perror("execvp");
-            exit(1);
+            exit();
         } else {
             wait(NULL);
         }
