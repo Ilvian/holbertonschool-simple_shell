a575eefce3e0b0e03899aabfb9035a1f57bb296c
Arlind3553 <georgetesco@yahoo.com>
2023-08-17 04:04:38 -0700

    task 1

--- myshell.c
+++ myshell.c
@@ -28,7 +28,7 @@ int main() {
         args[argc] = NULL;
         if (argc > 0) {
             if (strcmp(args[0], "exit") == 0) {
-                exit(127);
+                exit(0);
             } else if (strcmp(args[0], "echo") == 0 && argc == 2 && strcmp(args[1], "$$") == 0) {
                 printf("Shell Process ID: %d\n", getpid());
                 continue;
