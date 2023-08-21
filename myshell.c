296392179b6134d42815ce9387ff340fca80c2f4
Arlind3553 <georgetesco@yahoo.com>
2023-08-21 01:59:51 -0700

    task 1

--- myshell.c
+++ myshell.c
@@ -3,10 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <errno.h>
-
 #define MAX_INPUT_LENGTH 1024
-
 int main(int argc, char *argv[]) {
     char input[MAX_INPUT_LENGTH];
     char *args[MAX_INPUT_LENGTH / 2];
@@ -14,20 +11,17 @@ int main(int argc, char *argv[]) {
     int i;
     pid_t pid;
     char *token;
-    int last_exit_status = 0; 
-
+    char *input_copy;
     (void) argc;
-
     while (1) {
         if (isatty(fileno(input_stream))) {
             printf("MyShell> ");
         }
-
         if (fgets(input, sizeof(input), input_stream) == NULL) {
             break;
         }
-
         input[strcspn(input, "\n")] = '\0';
+        input_copy = strdup(input);
         i = 0;
         token = strtok(input, " ");
         while (token != NULL) {
@@ -35,16 +29,14 @@ int main(int argc, char *argv[]) {
             token = strtok(NULL, " ");
         }
         args[i] = NULL;
-
-	if (i > 0) {
-	    if (strcmp(args[0], "exit") == 0) {
-        if (last_exit_status == 0 && errno == ENOENT) {
-	            exit(2); 
- 	       }
- 	       exit(last_exit_status); 
- 	   }
-	}
-             else if (strcmp(args[0], "echo") == 0 && i == 2 && strcmp(args[1], "$$") == 0) {
+        if (args == NULL) {
+            continue;
+        }
+        if (i > 0) {
+            if (strcmp(args[0], "exit") == 0){
+		    free(input_copy);
+                exit(0);
+            } else if (strcmp(args[0], "echo") == 0 && i == 2 && strcmp(args[1], "$$") == 0) {
                 printf("Shell Process ID: %d\n", getpid());
                 continue;
             } else if (strcmp(args[0], "echo") == 0 && i == 2 && strcmp(args[1], "$PATH") == 0) {
@@ -52,19 +44,18 @@ int main(int argc, char *argv[]) {
                 continue;
             }
         }
-
         pid = fork();
         if (pid == -1) {
             perror("fork");
         } else if (pid == 0) {
-            if (execvp(args[0], args)) {
-                fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
-                exit(127);
-            }
+            if(execvp(args[0], args))
+            {
+            printf("%s: 1: %s: not found\n", argv[0] ,args[0]);
+            exit(127);}
         } else {
-            wait(&last_exit_status); 
+            wait(NULL);
+            free(input_copy);
         }
-    
-
+    }
     return 0;
 }
