9d575cb3247caa375e3f2678bca618563431afd6
Ilvian <d.ilvian1993@gmail.com>
2023-08-22 05:08:08 -0700

    shell

--- myshell.c
+++ myshell.c
@@ -1,90 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <ctype.h>
 
-#define MAX_INPUT_LENGTH 1024
-#define PROMPT "#cisfun$ "
+char *trim_whitespace(char *str);
+void execute_command(char *command);
+
+int main(void) {
+    char *input = NULL;
+    size_t len = 0;
+    
+    while (true) {
+        printf("#cisfun$ ");
+        getline(&input, &len, stdin);
+
+        if (feof(stdin)) {
+            printf("\n");
+            free(input);
+            exit(0);
+        }
+
+        input[strcspn(input, "\n")] = 0;
+        execute_command(input);
+    }
+
+    free(input);
+    return 0;
+}
 
 char *trim_whitespace(char *str) {
     char *end;
-
     while(isspace((unsigned char)*str)) str++;
 
-    if(*str == 0)
+    if(*str == 0) 
         return str;
 
     end = str + strlen(str) - 1;
     while(end > str && isspace((unsigned char)*end)) end--;
-
     end[1] = '\0';
-
     return str;
 }
 
-int main(void) {
-    char input[MAX_INPUT_LENGTH];
-    char path_command[MAX_INPUT_LENGTH];
-    char *trimmed_input;
-    char *token;
-    char *args[MAX_INPUT_LENGTH / 2];
-    int i, status;
-    pid_t pid;
+void execute_command(char *command) {
+    pid_t pid, wpid;
+    int status;
+    char *args[3];
 
-    while (1) {
-        if (isatty(STDIN_FILENO)) {
-            printf(PROMPT);
-            fflush(stdout);
+    command = trim_whitespace(command);
+    args[0] = "/bin/";
+    args[1] = command;
+    args[2] = NULL;
+
+    pid = fork();
+
+    if (pid == 0) {
+        if (execv(args[0], args) == -1) {
+            perror("./hsh");
+            exit(EXIT_FAILURE);
         }
+    } else if (pid < 0) {
+        perror("./hsh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            exit(0);
-        }
-
-        input[strcspn(input, "\n")] = '\0';
-        trimmed_input = trim_whitespace(input);
-
-        if (strlen(trimmed_input) == 0) {
-            continue;
-        }
-
-        i = 0;
-        token = strtok(trimmed_input, " ");
-        while(token != NULL) {
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        args[i] = NULL;
-
-        if (args[0][0] == '.' || args[0][0] == '/') {
-            snprintf(path_command, sizeof(path_command), "%s", args[0]);
-        } else {
-            snprintf(path_command, sizeof(path_command), "/bin/%s", args[0]);
-        }
-
-        args[0] = path_command;
-
-        pid = fork();
-        if (pid == 0) {
-            if (execv(args[0], args) == -1) {
-                fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-                exit(127);
-            }
-        } else {
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status)) {
-                int exit_status = WEXITSTATUS(status);
-                if (exit_status == 127) {
-                    exit(127);
-                }
-            }
+        if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE) {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", command);
+            exit(127);
         }
     }
-
-    return 0;
 }
 
