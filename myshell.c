aa4026cf26e4b821921e2e8baca52b5064c9e480
Ilvian <d.ilvian1993@gmail.com>
2023-08-22 05:11:13 -0700

    shell

--- myshell.c
+++ myshell.c
@@ -1,52 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
-
-char *trim_whitespace(char *str);
-void execute_command(char *command);
-
-int main(void) {
-    char *input = NULL;
-    size_t len = 0;
-    
-    while (true) {
-        printf("#cisfun$ ");
-        getline(&input, &len, stdin);
-
-        if (feof(stdin)) {
-            printf("\n");
-            free(input);
-            exit(0);
-        }
-
-        input[strcspn(input, "\n")] = 0;
-        execute_command(input);
-    }
-
-    free(input);
-    return 0;
-}
 
 char *trim_whitespace(char *str) {
     char *end;
-    while(isspace((unsigned char)*str)) str++;
 
-    if(*str == 0) 
+    while (isspace((unsigned char)*str)) str++;
+
+    if (*str == 0)
         return str;
 
     end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
+    while (end > str && isspace((unsigned char)*end)) end--;
+
     end[1] = '\0';
+
     return str;
 }
 
 void execute_command(char *command) {
-    pid_t pid, wpid;
+    pid_t pid;
     int status;
     char *args[3];
 
@@ -65,14 +42,35 @@ void execute_command(char *command) {
     } else if (pid < 0) {
         perror("./hsh");
     } else {
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        while (!WIFEXITED(status) && !WIFSIGNALED(status)) {
+            waitpid(pid, &status, WUNTRACED);
+        }
 
         if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE) {
             fprintf(stderr, "./hsh: 1: %s: not found\n", command);
             exit(127);
         }
     }
+}
+
+int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    while (1) {
+        printf("#cisfun$ ");
+        nread = getline(&line, &len, stdin);
+
+        if (nread == -1) {
+            free(line);
+            break;
+        }
+
+        line[nread - 1] = '\0';
+        execute_command(line);
+    }
+
+    return 0;
 }
 
