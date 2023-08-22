69bd71e0f7d573c3f76b440e6be76eb8551d5c39
Ilvian <d.ilvian1993@gmail.com>
2023-08-22 02:40:32 -0700

    shell

--- myshell.c
+++ myshell.c
@@ -3,9 +3,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define PROMPT "#cisfun$ "
 #define MAX_INPUT_LENGTH 1024
+
+char *trim_whitespace(char *str) {
+    char *end;
+
+    while(isspace((unsigned char)*str)) str++;
+
+    if(*str == 0) 
+        return str;
+
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) end--;
+
+    end[1] = '\0';
+
+    return str;
+}
 
 int main(void) {
     char input[MAX_INPUT_LENGTH];
@@ -22,14 +39,19 @@ int main(void) {
         }
 
         input[strcspn(input, "\n")] = '\0';
-        pid = fork();
 
-        if (pid == 0) {  
+        char *trimmed_input = trim_whitespace(input);
+        if (strlen(trimmed_input) == 0) {
+            continue;
+        }
+
+        pid = fork();
+        if (pid == 0) {
             char *args[2];
-            args[0] = input;
+            args[0] = trimmed_input;
             args[1] = NULL;
             
-            if (execve(input, args, NULL) == -1) {
+            if (execve(trimmed_input, args, NULL) == -1) {
                 printf("./shell: No such file or directory\n");
                 exit(1);
             }
